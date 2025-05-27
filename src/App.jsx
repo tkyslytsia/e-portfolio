a3407631a727942ddbd240e030561c0577cb5ce1
Tetiana <150958347+tkyslytsia@users.noreply.github.com>
2025-05-27 17:08:01 +0000

    add homepage path

--- src/App.jsx
+++ src/App.jsx
@@ -100,7 +100,7 @@ function AppContent() {
 
       <main>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/e-portfolio" element={<Home />} />
           <Route path="/portfolio" element={<Portfolio />} />
           <Route path="/about" element={<AboutMe />} />
           <Route path="/kontakt" element={<Kontakt />} />
